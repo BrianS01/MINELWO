1d850feefbe9ffff004133c030a64e3fa29ac286
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-05-01 18:34:15 -0500

    Inicio

--- src/main/java/vistas/entradaSencillo.java
+++ src/main/java/vistas/entradaSencillo.java
@@ -5,6 +5,8 @@
  */
 package vistas;
 
+import javax.swing.ImageIcon;
+
 public class EntradaSencillo extends javax.swing.JFrame
 {
     public EntradaSencillo()
@@ -12,6 +14,7 @@ public class EntradaSencillo extends javax.swing.JFrame
         initComponents();
         this.setTitle("MUNDULERY ~ Entrada Sencillo");
         setLocationRelativeTo(null);
+        this.setIconImage(new ImageIcon(getClass().getResource("/icono.png")).getImage());
     }
 
     /**
