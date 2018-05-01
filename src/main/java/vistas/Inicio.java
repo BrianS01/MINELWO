f0ff9c1feb31ebb652cb7aec8c1c50ed50458cbb
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-05-01 17:15:57 -0500

    Inicio

--- src/main/java/vistas/Inicio.java
+++ src/main/java/vistas/Inicio.java
@@ -35,7 +35,6 @@ public class Inicio extends JFrame
         jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setIconImage(new javax.swing.ImageIcon(getClass().getResource("icono.png")).getImage());
 
         jLabel1.setFont(new java.awt.Font("Dialog", 0, 24)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
