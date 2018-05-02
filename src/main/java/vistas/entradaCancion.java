51fd3ae86fa6005622a02cd5f86b66a8f45343ec
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-05-02 12:01:10 -0500

    Inicio

--- src/main/java/vistas/entradaCancion.java
+++ src/main/java/vistas/entradaCancion.java
@@ -5,6 +5,8 @@
  */
 package vistas;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import javax.swing.ImageIcon;
 
 public class EntradaCancion extends javax.swing.JFrame
@@ -12,9 +14,11 @@ public class EntradaCancion extends javax.swing.JFrame
     public EntradaCancion()
     {
         initComponents();
-        setLocationRelativeTo(null);
         this.setTitle("MUNDULERY ~ Entrada Cancion");
+        this.setMinimumSize(new Dimension(748, 540));
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         this.setIconImage(new ImageIcon(getClass().getResource("/icono.png")).getImage());
+        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
     }
 
     /**
