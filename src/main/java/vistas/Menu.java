1c612cdbe2a45969c9f7b69615190255653e2829
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-05-04 23:00:24 -0500

    Inicio

--- src/main/java/vistas/Menu.java
+++ src/main/java/vistas/Menu.java
@@ -115,7 +115,8 @@ public class Menu extends javax.swing.JFrame
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String args[])
+    {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -140,8 +141,10 @@ public class Menu extends javax.swing.JFrame
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
+        java.awt.EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
                 new Menu().setVisible(true);
             }
         });
