8f387e18818a3034ca87783f8eecd2d66dec77c7
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-05-04 22:55:37 -0500

    Inicio

--- src/main/java/vistas/MenuHistorial.java
+++ src/main/java/vistas/MenuHistorial.java
@@ -9,9 +9,9 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import javax.swing.ImageIcon;
 
-public class MenuVista extends javax.swing.JFrame
+public class MenuHistorial extends javax.swing.JFrame
 {
-    public MenuVista()
+    public MenuHistorial()
     {
         initComponents();
         this.setTitle("MUNDULERY ~ Vistas");
@@ -97,20 +97,21 @@ public class MenuVista extends javax.swing.JFrame
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MenuVista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuHistorial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MenuVista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuHistorial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MenuVista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuHistorial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MenuVista.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MenuHistorial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MenuVista().setVisible(true);
+                new MenuHistorial().setVisible(true);
             }
         });
     }
