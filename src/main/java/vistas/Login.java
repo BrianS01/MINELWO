b5fc5d968cf061aad871f9518baa4be3860c24d9
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-05-04 23:00:21 -0500

    Inicio

--- src/main/java/vistas/Login.java
+++ src/main/java/vistas/Login.java
@@ -145,7 +145,8 @@ public class Login extends javax.swing.JFrame
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String args[])
+    {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -170,8 +171,10 @@ public class Login extends javax.swing.JFrame
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
+        java.awt.EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
                 new Login().setVisible(true);
             }
         });
