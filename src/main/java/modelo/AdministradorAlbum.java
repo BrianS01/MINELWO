202effa4a39b5aaa49cee724e59fcf538edc3299
cris <crisspidey8@gmail.com>
2018-05-19 17:57:04 -0500

    cambios

--- src/main/java/modelo/AdministradorAlbum.java
+++ src/main/java/modelo/AdministradorAlbum.java
@@ -1,12 +1,15 @@
 package modelo;
 
 import BasesDeDatos.Conexion;
+import com.sun.istack.internal.logging.Logger;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import vo.Album;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
 
 public class AdministradorAlbum
 {
@@ -32,21 +35,46 @@ public class AdministradorAlbum
             
             Album album;
             
-        } catch (Exception e) {
+            while(rs.next()){
+                album=new Album(rs.getInt(1), rs.getString(2));
+                albumes.add(album);
+            }
+        } catch (SQLException ex) {
+            //Logger.getLogger(AdministradorInterprete.class.getName()).log(Level.SEVERE, null, ex);
+            
+          //  Logger.getLogger(AdministradorAlbum.class.getName().log(Level.SEVERE, null, ex));
+        } finally{
+            try {  
+                if (rs != null) {
+                    rs.close();
+                }
+                if (ps != null) {
+                    ps.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+                
+            } catch (SQLException ex) {
+       //         Logger.getLogger(AdministradorAlbum.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    
         }
-        
         
         return albumes;
     }
-    /*
-    public boolean ingresarAlbum(int idAlbum, String nombreAlbum, String nombreInterprete){
-        for (int i = 0; i < 10; i++) {
+    
+    public boolean ingresarAlbum(String nombreAlbum){
+        
+      //  for (int i = 0; i < 10; i++) {
 //            mialbum=new Album(i, nombreAlbum, misInterpretes.getInterpretes().get(i).getNombreInterprete());
             //misInterpretes.getInterpretes().get(i+1).getNombreInterprete()   
-        }
-        return listadeAlbunes.add(mialbum);
+       // }
+       // return listadeAlbunes.add(mialbum);
+       return true; 
     }
     
+    /*
     public static AdministradorAlbum getInstance(){
         if(administradorAlbum == null){
             return new AdministradorAlbum();
