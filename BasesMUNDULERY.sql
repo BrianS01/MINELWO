cfe4c20090f05bd28ac3b7b919c2592c365d193b
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-05-22 19:35:58 -0500

    Alteración

--- BasesMUNDULERY.sql
+++ BasesMUNDULERY.sql
@@ -22,41 +22,41 @@ CREATE TABLE IF NOT EXISTS MUNDULERY.Sencillo
 
 CREATE TABLE IF NOT EXISTS MUNDULERY.Album
 (
-       idAlbum INT NOT NULL,
-   nombreAlbum VARCHAR(45) NULL,
-  idInterprete INT NULL,
-  PRIMARY KEY (idAlbum),
-  FOREIGN KEY (idInterprete) REFERENCES MUNDULERY.Interprete(idInterprete)
+	     idAlbum INT NOT NULL,
+     nombreAlbum VARCHAR(45) NULL,
+    idInterprete INT NULL,
+    PRIMARY KEY (idAlbum),
+    FOREIGN KEY (idInterprete) REFERENCES MUNDULERY.Interprete(idInterprete)
 )ENGINE = InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS MUNDULERY.Cancion
 (
-      idCancion INT NOT NULL,
-  nombreCancion VARCHAR(45) NULL,
-        idAlbum INT NULL,
-  PRIMARY KEY (idCancion),
-  FOREIGN KEY (idAlbum) REFERENCES MUNDULERY.Album(idAlbum)
+	    idCancion INT NOT NULL,
+    nombreCancion VARCHAR(45) NULL,
+          idAlbum INT NULL,
+    PRIMARY KEY (idCancion),
+    FOREIGN KEY (idAlbum) REFERENCES MUNDULERY.Album(idAlbum)
 )ENGINE = InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS MUNDULERY.VentaSencillo
 (
-  idVentaSencillo INT NOT NULL,
-            fecha DATE NULL,
-         cantidad INT NULL,
-       idSencillo INT NULL,
-  PRIMARY KEY (idVentaSencillo),
-  FOREIGN KEY (idSencillo) REFERENCES MUNDULERY.Sencillo(idSencillo)
+	idVentaSencillo INT NOT NULL,
+              fecha DATE NULL,
+           cantidad INT NULL,
+         idSencillo INT NULL,
+	PRIMARY KEY (idVentaSencillo),
+    FOREIGN KEY (idSencillo) REFERENCES MUNDULERY.Sencillo(idSencillo)
 )ENGINE = InnoDB;
 
 
 CREATE TABLE IF NOT EXISTS MUNDULERY.VentaAlbum
 (
-  idVentaAlbum INT NOT NULL,
-  fecha DATE NULL,
-  cantidad INT NULL,
-  idAlbum INT NULL,
-  PRIMARY KEY (idVentaAlbum),
-  FOREIGN KEY (idAlbum) REFERENCES MUNDULERY.Album(idAlbum)
+	idVentaAlbum INT NOT NULL,
+           fecha DATE NULL,
+        cantidad INT NULL,
+         idAlbum INT NULL,
+    PRIMARY KEY (idVentaAlbum),
+    FOREIGN KEY (idAlbum) REFERENCES MUNDULERY.Album(idAlbum)
 )ENGINE = InnoDB;
