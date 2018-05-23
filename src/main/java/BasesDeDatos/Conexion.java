e59cbcfb50687db86913bd80117620a58ef9d770
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-05-22 19:36:45 -0500

    Alteración

--- src/main/java/BasesDeDatos/Conexion.java
+++ src/main/java/BasesDeDatos/Conexion.java
@@ -8,18 +8,26 @@ public class Conexion {
 
     private Connection cnx = null;
 
-    public Connection obtener() throws SQLException {
-        if (cnx == null || cnx.isClosed()) {
-            try {
+    public Connection obtener() throws SQLException
+    {
+        if (cnx == null || cnx.isClosed())
+        {
+            try
+            {
                 Class.forName("com.mysql.jdbc.Driver");
                 cnx = DriverManager.getConnection("jdbc:mysql://localhost/mundulery", "root", "nogamenolife");
 
-                if (cnx != null) {
+                if (cnx != null)
+                {
                     System.out.println("Conexión realizada con exito");
                 }
-            } catch (SQLException ex) {
+            }
+            catch (SQLException ex)
+            {
                 System.err.println("Se presento una SQLException");
-            } catch (ClassNotFoundException ex) {
+            }
+            catch (ClassNotFoundException ex)
+            {
                 System.err.println("Se presento una ClassNotFoundException");
             }
         }
@@ -27,8 +35,11 @@ public class Conexion {
         return cnx;
     }
 
-    public void cerrar() throws SQLException {
-        if (cnx != null) {
+    
+    public void cerrar() throws SQLException
+    {
+        if (cnx != null)
+        {
             cnx.close();
         }
     }
