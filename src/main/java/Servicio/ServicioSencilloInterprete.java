0d6f2b7f2db5ff713e094ab54c7d2d342b280c3b
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-05-22 20:04:47 -0500

    Alteración

--- src/main/java/Servicio/ServicioSencilloInterprete.java
+++ src/main/java/Servicio/ServicioSencilloInterprete.java
@@ -3,16 +3,15 @@ package Servicio;
 import modelo.AdministradorInterprete;
 import modelo.AdministradorSencillo;
 
-/**
- *
- * @author cristian
- */
-public class ServicioSencilloInterprete {
+public class ServicioSencilloInterprete
+{
     AdministradorInterprete administradorinterprete;
     AdministradorSencillo administradorSencillo;
       
+    
     //idSencillo, nombreSencillo, List<Interprete> interpretes--> String nombreInterprete
-    public void agregarInterpreteSencillo(){
+    public void agregarInterpreteSencillo()
+    {
         administradorSencillo.obtenerSencillos();
     }
 }
