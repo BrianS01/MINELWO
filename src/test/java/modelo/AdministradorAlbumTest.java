571e90b8be4e8d1f0752ebd95b90a72bc7b94e66
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-05-25 21:09:21 -0500

    Alteración

--- src/test/java/modelo/AdministradorAlbumTest.java
+++ src/test/java/modelo/AdministradorAlbumTest.java
@@ -39,17 +39,15 @@ public class AdministradorAlbumTest
     {
     }
     
+    
     @Before
-    public void setUp() {
+    public void setUp()
+    {
     }
     
+    
     @After
-    public void tearDown() {
+    public void tearDown()
+    {
     }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
