fda4d202029f1ffaa73f2fcb3ee33ca3bafc9914
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-05-30 13:51:20 -0500

    Alteración

--- src/main/java/Servicio/ServicioSencilloInterprete.java
+++ src/main/java/Servicio/ServicioSencilloInterprete.java
@@ -1,3 +1,11 @@
+/*
+ *  PROYECTO TERCER CORTE
+ *   co-Author ::: Cristian Espinosa
+ *   co-Author ::: Brian Sterling
+ *     Program ::: Ingenieria de Software II
+ *  Credential ::: SIST0076-G01:SIVII
+ */
+
 package Servicio;
 
 import modelo.AdministradorInterprete;
